711e9b6409240c078c5be8d59edd211c71c56e92
Patric Pauluk <patric.pauluk@outlook.com>
2025-01-10 20:25:00 -0300

    created controllers to main page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  # Mapeamento, traduzindo o caminho de welcome#index como inicio.
+  # Acessa como: http://localhost:3000/inicio
+  get 'inicio', to: 'welcome#index'
+
+  # Define a rota padrão para Welcome Index
+  root to: 'welcome#index' # nome_controler#pagina
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
