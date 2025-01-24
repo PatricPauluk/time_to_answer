924046bc0086a0460e2e901fbc609f75b192740c
Patric Pauluk <patric.pauluk@outlook.com>
2025-01-24 18:43:29 -0300

    Adjusting homepage layout.

--- app/controllers/welcome_controller.rb
+++
@@ -1,8 +0,0 @@
-class WelcomeController < ApplicationController
-
-  # Autenticação básica do HTTP, solicitando um login e senha
-  # http_basic_authenticate_with name: "patric", password: "123"
-
-  def index
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
   namespace :profiles_backoffice do
     get 'welcome/index'
   end
@@ -7,14 +10,13 @@ Rails.application.routes.draw do
   end
   devise_for :profiles
   devise_for :admins
-  get 'welcome/index'
 
-  # Mapeamento, traduzindo o caminho de welcome#index como inicio.
+  # Mapeamento, traduzindo o caminho de site/welcome#index como inicio.
   # Acessa como: http://localhost:3000/inicio
-  get 'inicio', to: 'welcome#index'
+  get 'inicio', to: 'site/welcome#index'
 
   # Define a rota padrão para Welcome Index
-  root to: 'welcome#index' # nome_controler#pagina
+  root to: 'site/welcome#index' # nome_controler#pagina
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
