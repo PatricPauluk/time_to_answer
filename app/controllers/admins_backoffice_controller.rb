09fd34f2fbf3a0f2df7b374529e9c9be1b7c33e1
Patric Pauluk <patric.pauluk@outlook.com>
2025-01-24 19:32:04 -0300

    Using Devise for backoffice authentication.

--- app/controllers/admins_backoffice_controller.rb
+++ app/controllers/admins_backoffice_controller.rb
@@ -1,4 +1,7 @@
 class AdminsBackofficeController < ApplicationController
+  # Especifica que vai proteger todo controller que herdar deste controller (no caso, a pasta app/controllers/admins_backoffice)
+  before_action :authenticate_admin!
+
   # Especifica que vai utilizar o layout app/views/layouts/admins_backoffice.html.erb
   layout 'admins_backoffice'
 end
--- app/controllers/profiles_backoffice_controller.rb
+++ app/controllers/profiles_backoffice_controller.rb
@@ -1,4 +1,7 @@
 class ProfilesBackofficeController < ApplicationController
+  # Especifica que vai proteger todo controller que herdar deste controller (no caso, a pasta app/controllers/profiless_backoffice)
+  before_action :authenticate_profile!
+  
   # Especifica que vai utilizar o layout app/views/layouts/profiles_backoffice.html.erb
   layout 'profiles_backoffice'
 end
