45ec2b121bf60701f031177110237aafcf206d3f
Patric Pauluk <patric.pauluk@outlook.com>
2025-03-21 14:51:32 -0300

    Enhancing i18n support for models.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+    # Helper para traduzir os campos
+    def translate_field(object = nil, method = nil)
+        (object && method) ? object.model.human_attribute_name(method) : "Informe os parâmetros corretamente."
+    end
 end
