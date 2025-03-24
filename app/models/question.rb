1aba334c884775e037bc691cfeb739bed4a883cf
Patric Pauluk <patric.pauluk@outlook.com>
2025-03-23 22:36:29 -0300

    Beginning implementation of answers as nested attributes in questions.

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :subject # uma pergunta pertence a um assunto
+  has_many :answers # uma pergunta tem muitas respostas
+  
+  accept_nested_attributes_for :answers # aceita respostas aninhadas
 end
