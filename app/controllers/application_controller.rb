3760bf1d2fe2b3e611754f8482edd0ef1c509933
Patric Pauluk <patric.pauluk@outlook.com>
2025-03-24 22:56:00 -0300

    Preparing environment for admin Devise template configuration by checking for admin controllers.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+    layout :layout_by_resource
+
+    protected
+    
+    # devise_controller? verifica se o controller atual pertence ao devise
+    # resource_class verifica qual classe de devise o controller atual pertence (Admin ou User)
+    def layout_by_resource
+        if devise_controller? && resource_class == Admin
+            "admin_devise"
+        else
+            "application"
+        end
+    end
 end
