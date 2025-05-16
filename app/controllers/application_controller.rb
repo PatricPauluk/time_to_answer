0bf31ad80af3f2d6ba7b816f29cb03ce95fcfba7
Patric Pauluk <patric.pauluk@outlook.com>
2025-05-15 23:10:08 -0300

    Restricted question visibility to logged-in users only.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
     layout :layout_by_resource
+    before_action :check_pagination
 
     protected
     
@@ -8,5 +9,12 @@ class ApplicationController < ActionController::Base
     # ... e retorna o layout correspondente
     def layout_by_resource
         devise_controller? ? "#{resource_class.to_s.downcase}_devise" : "application"
+    end
+
+    # Remove o parâmetro page caso o usuário não esteja logado, para ver as outras questões
+    def check_pagination
+        unless user_signed_in?
+            params.extract!(:page)
+        end
     end
 end
