3395ed0b757688f0c44f7dfeb4c5baebb483b8e0
Patric Pauluk <patric.pauluk@outlook.com>
2025-05-20 23:47:50 -0300

    Displaying user statistics for answered questions.

--- app/controllers/users_backoffice/welcome_controller.rb
+++ app/controllers/users_backoffice/welcome_controller.rb
@@ -2,5 +2,6 @@
 class UsersBackoffice::WelcomeController < UsersBackofficeController
   # O layout foi especificado novamente em app/controllers/users_backoffice_controller.rb
   def index
+    @user_statistic = UserStatistic.find_or_create_by(user: current_user)
   end
 end
