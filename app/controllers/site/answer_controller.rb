2dbdcc62e64700778644599a99c5247744f8f197
Patric Pauluk <patric.pauluk@outlook.com>
2025-06-04 11:08:56 -0300

    Refactored UserStatistic to centralize the logic for setting user answer statistics in a class method.

--- app/controllers/site/answer_controller.rb
+++ app/controllers/site/answer_controller.rb
@@ -1,15 +1,10 @@
 class Site::AnswerController < SiteController
     def question
         @answer = Answer.find(params[:answer_id])
-        set_user_statistic(@answer)
+
+        # O método foi passado para o helper, e para funcionar precisa de informações como o usuário atual e se ele esta logado
+        UserStatistic.set_statistic(@answer, current_user)
     end
 
-    # Caso o usuário estiver logado, incrementa o campo de respostas corretas ou erradas
-    def set_user_statistic(answer)
-        if user_signed_in?
-            user_statistic = UserStatistic.find_or_create_by(user: current_user)
-            @answer.correct? ? user_statistic.right_questions += 1 : user_statistic.wrong_questions += 1
-            user_statistic.save
-        end
-    end
+
 end
--- app/models/user_statistic.rb
+++ app/models/user_statistic.rb
@@ -5,4 +5,15 @@ class UserStatistic < ApplicationRecord
   def total_questions
     self.right_questions + self.wrong_questions
   end
+
+  # Métodos de classe
+  # Caso o usuário estiver logado, incrementa o campo de respostas corretas ou erradas
+  def self.set_statistic(answer, current_user)
+      if !!current_user # É o mesmo que user_signed_in
+          user_statistic = UserStatistic.find_or_create_by(user: current_user)
+          answer.correct? ? user_statistic.right_questions += 1 : user_statistic.wrong_questions += 1
+          user_statistic.save
+      end
+  end
+
 end
